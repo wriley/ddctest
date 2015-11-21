3295cbc1a8a946d4e7fc447523484993d0cc5c3b
William Riley <wrileystl@gmail.com>
2015-11-21 17:33:06 -0600

    more debug

--- ddctest/main.c
+++ ddctest/main.c
@@ -51,13 +51,38 @@ PROGMEM const char usbHidReportDescriptor[22] = {   /* USB report descriptor */
  * instead.
  */
 
+static uchar reportBuffer[8];
+static uchar    idleRate;   /* repeat rate for keyboards, never used for mice */
+
 /* ------------------------------------------------------------------------- */
 
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
 usbRequest_t    *rq = (void *)data;
-	DBG1(0x40, data, 8);
-    return 0;
+
+    /* The following requests are never used. But since they are required by
+     * the specification, we implement them in this example.
+     */
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
+        DBG1(0x50, &rq->bRequest, 1);   /* debug output: print our request */
+        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+			DBG1(0x51, 0, 1);
+            /* we only have one report type, so don't look at wValue */
+            usbMsgPtr = (void *)&reportBuffer;
+            return sizeof(reportBuffer);
+        }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
+			DBG1(0x52, 0, 1);
+            usbMsgPtr = &idleRate;
+            return 1;
+        }else if(rq->bRequest == USBRQ_HID_SET_IDLE){
+			DBG1(0x53, 0, 1);
+            idleRate = rq->wValue.bytes[1];
+        }
+    }else{
+		DBG1(0x60, 0, 1);
+        /* no vendor specific requests implemented */
+    }
+    return 0;   /* default for not implemented requests: return no data back to host */
 }
 
 /* ------------------------------------------------------------------------- */
