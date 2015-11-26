aa6f20eeb12f95cd499ad67d6fee07d78f2d7df1
William Riley <wrileystl@gmail.com>
2015-11-25 19:47:43 -0600

    sort of working base

--- ddctest/main.c
+++ ddctest/main.c
@@ -33,61 +33,119 @@ different port or bit, change the macros below:
 /* ----------------------------- USB interface ----------------------------- */
 /* ------------------------------------------------------------------------- */
 
-PROGMEM const char usbHidReportDescriptor[22] = {   /* USB report descriptor */
-    0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
-    0x09, 0x01,                    // USAGE (Vendor Usage 1)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x09, 0x00,                    //   USAGE (Undefined)
-    0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
-    0xc0                           // END_COLLECTION
+PROGMEM const char usbHidReportDescriptor[54] = {   /* USB report descriptor */
+    0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined 0xFF00)
+    0x09, 0x01,        // Usage (0x01)
+    0xA1, 0x01,        // Collection (Application)
+    0x05, 0x08,        //   Usage Page (LEDs)
+    0x09, 0x52,        //   Usage (0x52)
+    0xA1, 0x02,        //   Collection (Logical)
+    0x19, 0x01,        //     Usage Minimum (Num Lock)
+    0x29, 0x14,        //     Usage Maximum (CAV)
+    0x15, 0x00,        //     Logical Minimum (0)
+    0x25, 0x01,        //     Logical Maximum (1)
+    0x85, 0x01,        //     Report ID (1)
+    0x75, 0x08,        //     Report Size (8)
+    0x95, 0x08,        //     Report Count (8)
+    0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0xC0,              //   End Collection
+    0x05, 0x08,        //   Usage Page (LEDs)
+    0x09, 0x52,        //   Usage (0x52)
+    0xA1, 0x02,        //   Collection (Logical)
+    0x19, 0x01,        //     Usage Minimum (Num Lock)
+    0x29, 0x14,        //     Usage Maximum (CAV)
+    0x15, 0x00,        //     Logical Minimum (0)
+    0x25, 0x01,        //     Logical Maximum (1)
+    0x85, 0x02,        //     Report ID (2)
+    0x75, 0x08,        //     Report Size (8)
+    0x95, 0x08,        //     Report Count (8)
+    0x91, 0x02,        //     Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0,              //   End Collection
+    0xC0              // End Collection
 };
-/* The descriptor above is a dummy only, it silences the drivers. The report
- * it describes consists of one byte of undefined data.
- * We don't transfer our data through HID reports, we use custom requests
- * instead.
- */
 
-static uchar reportBuffer[8];
-static uchar    idleRate;   /* repeat rate for keyboards, never used for mice */
+static uchar	reportOK[8] = { 0x10, 0x01, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00 };
+static uchar	reportVer[8] = { 0x30, 0x30, 0x30, 0x32, 0x30, 0x32, 0x30, 0x30 };
+static uchar	currentCmd[9];
+static uchar    currentAddress;
+static uchar    bytesRemaining;
 
 /* ------------------------------------------------------------------------- */
+
+/* usbFunctionRead() is called when the host requests a chunk of data from
+ * the device. For more information see the documentation in usbdrv/usbdrv.h.
+ */
+uchar   usbFunctionRead(uchar *data, uchar len)
+{
+	//TODO
+		
+	uchar i;
+	DBG1(0x80, currentCmd, 9);
+	
+	if(len == 1){
+		data[0] = 0x00;
+		return 1;
+	}
+	
+	if(currentCmd[0] == 0x02){
+		switch(currentCmd[1]){
+			case 0x0b:
+				for(i = 0; i < 8; i++){
+					data[i] = reportVer[i];
+				}
+				break;
+			default:
+				for(i = 0; i < 8; i++){
+					data[i] = reportOK[i];
+				}
+				break;
+		}
+	}
+	DBG1(0x81, data, len);
+    return len;
+}
+
+/* usbFunctionWrite() is called when the host sends a chunk of data to the
+ * device. For more information see the documentation in usbdrv/usbdrv.h.
+ */
+uchar   usbFunctionWrite(uchar *data, uchar len)
+{
+	uchar i;
+	
+	//TODO
+	DBG1(0x90, data, len);
+	for(i = 0; i < len; i++){
+		currentCmd[currentAddress] = data[i];
+		currentAddress++;
+		bytesRemaining--;
+	}
+	return bytesRemaining == 0; /* return 1 if this was the last chunk */
+}
 
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
 usbRequest_t    *rq = (void *)data;
 	DBG1(0x40, data, 8);
-    /* The following requests are never used. But since they are required by
-     * the specification, we implement them in this example.
-     */
-    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
-        DBG1(0x50, &rq->bRequest, 1);   /* debug output: print our request */
-        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
-			DBG1(0x51, 0, 1);
-            /* we only have one report type, so don't look at wValue */
-            usbMsgPtr = (void *)&reportBuffer;
-            return sizeof(reportBuffer);
-        }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
-			DBG1(0x52, &idleRate, 1);
-            usbMsgPtr = &idleRate;
-            return 1;
-        }else if(rq->bRequest == USBRQ_HID_SET_IDLE){
-			DBG1(0x53, &rq->wValue.bytes[1], 1);
-            idleRate = rq->wValue.bytes[1];
+
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){
+        if(rq->bRequest == USBRQ_HID_GET_REPORT){
+			return USB_NO_MSG;  /* use usbFunctionRead() to obtain data */
+			//usbMsgPtr = (void *)&reportOK;
+			//return sizeof(reportOK);
+        }else if(rq->bRequest == USBRQ_HID_SET_REPORT){
+			currentAddress = 0;
+			bytesRemaining = rq->wLength.bytes[0];
+			return USB_NO_MSG;  /* use usbFunctionWrite() to receive data from host */
         }
     }else{
 		DBG1(0x60, 0, 1);
-        /* no vendor specific requests implemented */
     }
     return 0;   /* default for not implemented requests: return no data back to host */
 }
 
 /* ------------------------------------------------------------------------- */
 
-int __attribute__((noreturn)) main(void)
+int main(void)
 {
 uchar   i;
 
